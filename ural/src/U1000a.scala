0c0df578c079feecfdc56c793c96b80545db4315
Jianghan <jhpooorman@gmail.com>
2014-04-09 21:56:21 +0800

    Start Ural use Scala

---
+++ ural/src/U1000a.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by jianghan on 14-4-9.
+ */
+object U1000a extends App {
+  println(readLine().split(" ").map(_.toInt).sum)
+}
---
+++ ural/src/U1000b.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by jianghan on 14-4-9.
+ */
+
+import java.util.Scanner
+
+object U1000b {
+  def main(args: Array[String]) {
+    val in = new Scanner(System.in)
+    println(in.nextInt() + in.nextInt())
+    in.close()
+  }
+}
