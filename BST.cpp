a6bfdbbc2f7722583e3c708087dbd1d86ca06e17
Jianghan <jianghan@xiaomi.com>
2016-11-14 21:31:16 +0800

    [Algo] Add Template: BST

---
+++ BST.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Tree {
+    public:
+        Tree() {
+            root = NULL;
+        }
+        Tree(vector<int> data) {
+            root = NULL;
+            for (int i=0; i<data.size(); ++i) {
+                insert(data[i]);
+            }
+        }
+        void insert(int val) {
+            root = insert(val, root);
+        }
+        void preorder() {
+            if (root == NULL) {
+                cout << "NULL" << endl;
+                return;
+            }
+            preorder(root);
+        }
+        ~Tree() {
+        }
+    private:
+        TreeNode *root;
+        void preorder(TreeNode *point) {
+            if (point == NULL) {
+                return;
+            }
+            cout << point->val << endl;
+            preorder(point->left);
+            preorder(point->right);
+        }
+        TreeNode *insert(int val, TreeNode *point) {
+            if (point == NULL) {
+                point = new TreeNode(val);
+                return point;
+            }
+            if (val < point->val) {
+                point->left = insert(val, point->left);
+            } else if (val > point->val) {
+                point->right = insert(val, point->right);
+            }
+            return point;
+        }
+};
+
+int main() {
+    vector<int> data = {5, 4, 6, 7, 8};
+    Tree tree(data);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(3);
+    tree.preorder();
+    return 0;
+}
